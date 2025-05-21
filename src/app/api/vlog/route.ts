363980f16f0485db28fa88d8e43bd1e4702a844f
Jaden <hyunbin.kim.3376@gmail.com>
2025-05-21 23:19:28 +0900

    feat: notion api 연결 완료

---
+++ src/app/api/vlog/route.ts
@@ -0,0 +1,30 @@
+import notion from '@/shared/lib/notion';
+import axios, { AxiosError } from 'axios';
+import { NextResponse } from 'next/server';
+import type { NextRequest } from 'next/server';
+
+export async function GET(request: NextRequest) {
+  try {
+    const databaseId = process.env.NOTION_DATABASE_ID;
+    console.log(databaseId);
+    // api 요청
+    const response = await notion.databases.query({
+      database_id: databaseId!,
+
+      sorts: [
+        {
+          property: 'title', // 데이터베이스의 제목 필드 이름
+          direction: 'ascending',
+        },
+      ],
+    });
+    // 통신 체크
+
+    // 결과 값 리턴
+    return NextResponse.json({
+      body: response,
+    });
+  } catch (error) {
+    return NextResponse.json(null);
+  }
+}
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,19 @@
+'use client';
 import { Section } from '@/shared/ui';
 import { Header } from '@/widgets/header';
+import axios from 'axios';
+import { useEffect } from 'react';
 
 export default function Home() {
+  const fetchData = async () => {
+    const res = await axios.get('/api/vlog');
+    console.log(res);
+  };
+
+  useEffect(() => {
+    fetchData();
+  }, []);
+
   return (
     <div className="mx-auto min-h-screen max-w-screen-xl px-6 py-12 font-sans md:px-12 md:py-16 lg:py-0">
       <div className="lg:flex lg:justify-between lg:gap-4">
