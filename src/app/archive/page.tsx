2d4bf58cf845984209fdce2247283f7de338f9aa
Jaden <hyunbin.kim.3376@gmail.com>
2025-06-01 01:37:32 +0900

    feat: archive 상세 페이지 main 태그 및 h1 태그 설정

--- src/app/archive/page.tsx
+++ src/app/archive/page.tsx
@@ -9,16 +9,14 @@ import { BackButton } from '@/shared/ui';
 export default async function page() {
   const initialData = await getPages({ pageSize: ARCHIVE_PAGE_SIZE, query: null });
   return (
-    <div className="mx-auto min-h-screen max-w-screen-xl px-6 py-12 md:px-12 md:py-16 lg:py-0">
+    <main className="mx-auto min-h-screen max-w-screen-xl px-6 py-12 md:px-12 md:py-16 lg:py-0">
       <div className="lg:py-24">
         <SearchBox />
         <ArchiveNav />
-        <main>
-          <BackButton label="Hyunbin" path="/" />
-          <h1 className="text-4xl font-bold tracking-tight text-slate-200 sm:text-5xl">Archive</h1>
-          <ArchiveList initialData={initialData.pages} />
-        </main>
+        <BackButton label="Hyunbin" path="/" />
+        <h1 className="text-4xl font-bold tracking-tight text-slate-200 sm:text-5xl">Archive</h1>
+        <ArchiveList initialData={initialData.pages} />
       </div>
-    </div>
+    </main>
   );
 }
