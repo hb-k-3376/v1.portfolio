fe8cff091534ff82ff1d57bbdb8bcf6fe9245da8
Jaden <hyunbin.kim.3376@gmail.com>
2025-08-08 21:20:44 +0900

    feat: archive 이미지 최적화

--- src/features/archive/ui/content-blocks/ImageBlock.tsx
+++ src/features/archive/ui/content-blocks/ImageBlock.tsx
@@ -1,11 +1,16 @@
+'use client';
+
 import { ImageProperty } from '@/shared/types';
 import { getImageUrl, getImageCaption } from '@/shared/utils';
+import { Loader2 } from 'lucide-react';
+import { useState } from 'react';
 
 interface IImageBlockProps {
   image: ImageProperty;
 }
 
 export const ImageBlock = ({ image }: IImageBlockProps) => {
+  const [isLoading, setIsLoading] = useState(true);
   const src = getImageUrl(image);
   const alt = getImageCaption(image);
 
@@ -15,7 +20,16 @@ export const ImageBlock = ({ image }: IImageBlockProps) => {
 
   return (
     <div className="flex justify-center py-5">
-      <img src={src} loading="lazy" alt={alt} />
+      {/* 로딩 스켈레톤 */}
+      {isLoading && <Loader2 className="animate-spin" size={30} />}
+      <img
+        src={src}
+        alt={alt}
+        loading="eager"
+        fetchPriority="high"
+        decoding="async"
+        onLoad={() => setIsLoading(false)}
+      />
     </div>
   );
 };
