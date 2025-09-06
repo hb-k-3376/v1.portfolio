37fec9897f39e5594d946e91376d1a356849aa0b
Jaden <hyunbin.kim.3376@gmail.com>
2025-09-07 02:15:51 +0900

    [style] 테일윈드 수정

--- src/features/portfolio/ui/ProjectCard.tsx
+++ src/features/portfolio/ui/ProjectCard.tsx
@@ -16,10 +16,10 @@ export const ProjectCard = ({
       <div className="group relative grid gap-4 pb-1 transition-all group-hover/list:opacity-50 hover:opacity-100 sm:grid-cols-8">
         <div className="absolute -inset-x-6 -inset-y-4 z-0 hidden rounded-md transition motion-reduce:transition-none lg:-inset-x-6 lg:block lg:group-hover:bg-slate-800/50 lg:group-hover:shadow-[inset_0_1px_0_0_rgba(148,163,184,0.1)] lg:group-hover:drop-shadow-lg" />
 
-        <div className="z-10 sm:col-span-6 flex flex-col gap-2 sm:order-2 order-1 text-sm leading-normal">
+        <div className="z-10 sm:col-span-6 flex flex-col gap-2 sm:order-2 order-1">
           <Title title={name} slug={link} />
-          <p>{description}</p>
-          <ul className="flex flex-col">
+          <p className="text-sm leading-normal">{description}</p>
+          <ul className="flex flex-col text-sm leading-normal">
             {roles.map((role, index) => (
               <li key={index}>• {role}</li>
             ))}
--- src/widgets/archive/_components/TableHeader.tsx
+++ src/widgets/archive/_components/TableHeader.tsx
@@ -1,19 +1,11 @@
 const TableHeader = () => {
   return (
     <thead className="sticky top-0 z-10 border-b border-slate-300/10 bg-slate-900/75 px-6 py-5 backdrop-blur">
-      <tr>
-        <th className="py-4 pr-8 w-1/12 text-sm font-semibold text-base">
-          created
-        </th>
-        <th className="py-4 pr-8 w-1/6 text-sm font-semibold text-base">
-          subject
-        </th>
-        <th className="hidden py-4 pr-8 w-1/5 text-sm font-semibold text-base md:table-cell">
-          tags
-        </th>
-        <th className="hidden py-4 w-1/4 pr-8 text-sm font-semibold text-base lg:table-cell">
-          desc
-        </th>
+      <tr className="text-sm font-semibold text-normal">
+        <th className="py-4 pr-8 w-1/12 ">created</th>
+        <th className="py-4 pr-8 w-1/6 ">subject</th>
+        <th className="hidden py-4 pr-8 w-1/5  md:table-cell">tags</th>
+        <th className="hidden py-4 w-1/4 pr-8  lg:table-cell">desc</th>
       </tr>
     </thead>
   );
