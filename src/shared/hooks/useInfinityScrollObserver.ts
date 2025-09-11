7106181ba2be778fd218f268c37f337d4d11f10b
Jaden <hyunbin.kim.3376@gmail.com>
2025-09-11 23:45:34 +0900

    [feat] 무한 스크롤 훅 useInfinityScrollObserver 구현

--- src/shared/hooks/useInfinityScrollObserver.ts
+++ src/shared/hooks/useInfinityScrollObserver.ts
@@ -11,6 +11,9 @@ const useInfinityScrollObserver = ({
   rootMargin = '200px',
   threshold = 0.1,
 }: Props) => {
+  /*
+  트리거 요소를 설정하고 해당 트리거 요소가 보이면 isIntersecting가 동작하면서 onIntersect가 실행됨
+  */
   const targetRef = useRef<HTMLDivElement>(null);
 
   useEffect(() => {
@@ -25,6 +28,7 @@ const useInfinityScrollObserver = ({
       { rootMargin, threshold }
     );
 
+    //targetRef 트리거 요소를 옵져브
     observer.observe(targetRef.current);
     return () => observer.disconnect();
   }, [onIntersect, rootMargin, threshold]);
