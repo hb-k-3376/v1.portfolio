98b81e6e463dec8ad7356e0cf02c7bd9707cc567
Jaden <hyunbin.kim.3376@gmail.com>
2025-09-14 02:25:57 +0900

    [style] callout block 아이콘 제거

--- src/features/archive/ui/content-blocks/CalloutBlock.tsx
+++ src/features/archive/ui/content-blocks/CalloutBlock.tsx
@@ -9,7 +9,7 @@ interface ICalloutBlockProps {
 export const CalloutBlock = ({ callout }: ICalloutBlockProps) => {
   return (
     <div className={`${getColorClass(callout.color)} rounded-lg p-5 flex my-5`}>
-      <div className="mr-5">{callout.icon.emoji}</div>
+      {/* <div className="mr-5">{callout.icon.emoji}</div> */}
       <ContentsText paragraph={callout} />
     </div>
   );
