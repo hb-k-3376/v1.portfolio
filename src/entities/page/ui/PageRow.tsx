1981992dac63e743efe91ca41b78328bf708f510
Jaden <hyunbin.kim.3376@gmail.com>
2025-10-24 01:17:42 +0900

    [style] 아키이브 테이블 반응형 적용

--- src/entities/page/ui/PageRow.tsx
+++ src/entities/page/ui/PageRow.tsx
@@ -1,22 +1,21 @@
 'use client';
 
 import Link from 'next/link';
+import { useRef } from 'react';
 import { Page } from '../model/';
 import { Badge } from '@/shared/ui';
-import { useRef } from 'react';
 
 /**
  *  Page 데이터를 row 형태로 보여주느 entity
  */
-export const PageRow = ({ created_time, description, tags, title, slug }: Page) => {
+export const PageRow = ({ created_time, tags, title, slug }: Page) => {
   const linkRef = useRef<HTMLAnchorElement | null>(null);
 
   const handleClick = (e: React.MouseEvent) => {
-    if (e.currentTarget.closest('a') || e.currentTarget.closest('button')) {
-      return;
-    }
     e.preventDefault();
-    linkRef.current?.click();
+    if (linkRef.current) {
+      linkRef.current.click();
+    }
   };
 
   return (
@@ -24,14 +23,14 @@ export const PageRow = ({ created_time, description, tags, title, slug }: Page) 
       className="group border-b border-slate-300/10 last:border-none cursor-pointer"
       onClick={handleClick}
     >
-      <td className="py-4 pr-4 text-sm">
-        <p className="whitespace-nowrap">{created_time}</p>
+      <td className="py-4 pr-4 align-top leading-tight text-sm">
+        <p>{created_time}</p>
       </td>
-      <td className="py-4 pr-4 leading-snug text-white">
+      <td className="py-4 pr-4 align-top leading-tight text-gray-50">
         <Link
           href={`/archive/${slug}`}
           ref={linkRef}
-          className="group-hover:text-primary focus-visible:text-primary items-baseline leading-tight whitespace-nowrap"
+          className="group-hover:text-primary focus-visible:text-primary leading-tight"
         >
           {title}
         </Link>
@@ -43,7 +42,7 @@ export const PageRow = ({ created_time, description, tags, title, slug }: Page) 
           ))}
         </ul>
       </td>
-      <td className="hidden py-4 pr-4 lg:table-cell max-w-[500px]">
+      <td className="hidden py-4 pr-4 align-top leading-tight text-sm lg:table-cell ">
         <p className="truncate">{slug}</p>
       </td>
     </tr>
