a5d7b1e235f7bde88b122cabfe7862b58c46a4ed
Jaden <hyunbin.kim.3376@gmail.com>
2025-10-28 08:58:13 +0900

    [refactor] img 태그 next Image 컴포넌트로 수정

--- src/features/archive/detail/ui/content-blocks/ImageBlock.tsx
+++ src/features/archive/detail/ui/content-blocks/ImageBlock.tsx
@@ -2,34 +2,37 @@
 
 import { ImageProperty } from '@/shared/types';
 import { getImageUrl, getImageCaption } from '@/shared/utils';
-import { Loader2 } from 'lucide-react';
+import Image from 'next/image';
 import { useState } from 'react';
 
-interface IImageBlockProps {
+interface Props {
   image: ImageProperty;
 }
 
-export const ImageBlock = ({ image }: IImageBlockProps) => {
-  const [isLoading, setIsLoading] = useState(true);
+export const ImageBlock = ({ image }: Props) => {
+  const [hasError, setHasError] = useState(false);
   const src = getImageUrl(image);
   const alt = getImageCaption(image);
 
-  if (!src) {
-    return null; // 또는 placeholder 이미지
+  if (!src || hasError) {
+    return null;
   }
 
   return (
-    <div className="flex justify-center py-5">
-      {/* 로딩 스켈레톤 */}
-      {isLoading && <Loader2 className="animate-spin" size={30} />}
-      <img
+    <div className="flex flex-col justify-center py-5">
+      <Image
         src={src}
         alt={alt}
-        loading="eager"
-        fetchPriority="high"
-        decoding="async"
-        onLoad={() => setIsLoading(false)}
+        height={400}
+        width={700}
+        quality={80}
+        className="object-cover transition-opacity duration-300"
+        onError={() => {
+          console.error('Image load failed:', src);
+          setHasError(true);
+        }}
       />
+      {alt && <p className="text-foreground text-sm">{alt}</p>}
     </div>
   );
 };
